886be64724c1679b82f8a10aef39596d09c2da4b
janki dave <jankidave995@gmail.com>
2026-01-22 21:28:43 +0530

    Add files via upload

---
+++ Task1.4.py
@@ -0,0 +1,22 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> p=float(input("enter a principle : "))
+enter a principle : 2.59
+>>> r=float(input("enter rate : "))
+enter rate : 25
+>>> t=float(input("enter a time :  "))
+enter a time :  40
+>>> si=(p*r*t)/100
+>>> print("simple interest = ",si)
+simple interest =  25.9
+>>> a=int(input("enter first number : "))
+enter first number : 20
+>>> b=int(input("enter second number "))
+enter second number 40
+>>> if a >b:
+...     print("maximum is ",a)
+... else:
+...     print("maximum is ",b)
+... 
+...     
+maximum is  40
