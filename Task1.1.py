72248f58f870562d54897e7aad92d1d5d899af60
janki dave <jankidave995@gmail.com>
2026-01-22 17:18:10 +0530

    Add files via upload

---
+++ Task1.1.py
@@ -0,0 +1,17 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("welcome to pyton prograaming!")
+welcome to pyton prograaming!
+>>> s=input("enter a string:")
+enter a string:2007
+>>> print("FIRST CHARACTER",S[0])
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print("FIRST CHARACTER",S[0])
+NameError: name 'S' is not defined. Did you mean: 's'?
+>>> print("first character",s[0])
+first character 2
+>>> s=input("enter a string:")
+enter a string:2006
+>>> print("last character= ",s[1])
+last character=  0
